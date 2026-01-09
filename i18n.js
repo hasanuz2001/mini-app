706aa90581859a89107dba0a8750f64fadac2fe2
hasanuz2001 <hasanuz2001@gmail.com>
2026-01-09 17:55:14 +0500

    Recreate i18n.js at root

---
+++ i18n.js
@@ -0,0 +1,6 @@
+window.translations = {
+  uz: { finish: "Rahmat!", thank_you: "So‘rovnoma yakunlandi." },
+  uz_cyrl: { finish: "Раҳмат!", thank_you: "Сўровнома якунланди." },
+  ru: { finish: "Спасибо!", thank_you: "Опрос завершён." },
+  en: { finish: "Thank you!", thank_you: "The survey is completed." }
+};
